21e563774eede7fbf7bfccffa00dc9dd14a1620a
justice725 <84830256+justice725@users.noreply.github.com>
2022-07-22 16:52:57 +0900

    07.22
    모바일 반응형 완료

--- js/introduce.js
+++ js/introduce.js
@@ -21,17 +21,20 @@ $(function(){
 
         var sec0Ht = $('.section0').height();
         var sec0_1 = $('.section0_1').offset().top;
+        var sec0_2 = $(".section0_2").offset().top; // 두번째 brand mission 애니메이션 높이
         var sec0_3 = $('.section0_3').offset().top;
         var sec0_3Height = $('.section0_3').height();
         var animate_1 = $(".animate_1").offset().top; // 첫번째 카드 애니메이션 높이 
         var animate_2 = $(".animate_2").offset().top; // 두번째 카드 애니메이션 높이 
         var animate_3 = $(".animate_3").offset().top; // 세번째 카드 애니메이션 높이 
-            console.log(ht-(animate_2-(wH/2)));
 
         var sec3_animate_1 = $(".sec3_animate_1").offset().top; // section3 첫번째 카드 애니메이션 높이
         var sec3_animate_2 = $(".sec3_animate_2").offset().top; // section3 두번째 카드 애니메이션 높이
         var sec3_animate_3 = $(".sec3_animate_3").offset().top; // section3 세번째 카드 애니메이션 높이
         var sec3_animate_4 = $(".sec3_animate_4").offset().top; // section3 네번째 카드 애니메이션 높이
+        var text_wrap1_1 = $(".animate_1 .text_wrap").offset().top; // 카드 텍스트 애니메이션 모바일 높이
+        var text_wrap1_2 = $(".animate_2 .text_wrap").offset().top; // 카드 텍스트 애니메이션 모바일 높이
+        var text_wrap1_3 = $(".animate_3 .text_wrap").offset().top; // 카드 텍스트 애니메이션 모바일 높이
 
         var who_we_are = $(".who_we_are").offset().top; // we are cenno top 애니메이션 높이
         var comment = $(".comment").offset().top; // we are cenno 텍스트 애니메이션 높이
@@ -39,24 +42,32 @@ $(function(){
         var value_2 = $(".value_2").offset().top; // we are cenno 두번째 카드 애니메이션 높이
         var value_3 = $(".value_3").offset().top; // we are cenno 세번째 카드 애니메이션 높이
 
-        var text_animate_1 = $(".text_animate_1").offset().top; // 하단 첫번째 텍스트 애니메이션 높이
-        var text_animate_2 = $(".text_animate_2").offset().top; // 하단 두번째 텍스트 애니메이션 높이
+        var text_animate_4 = $(".text_animate_4").offset().top; // 하단 첫번째 텍스트 애니메이션 높이
+        var text_animate_5 = $(".text_animate_5").offset().top; // 하단 두번째 텍스트 애니메이션 높이
+        var text_animate_3 = $(".text_animate_3").offset().top; // 하단 두번째 텍스트 애니메이션 높이
 
 
+        // 첫번째 brandmission 텍스트 좌우스크롤 애니메이션
         if(ht > 0) {
             $(".paralax_title").css({transform:"translateX(-"+ht / 20 + "%)"});
         };
+        // 첫번째 brandmission 텍스트 좌우스크롤 애니메이션 끝
+
+        // 두번째 brandmission 텍스트 좌우스크롤 애니메이션
         if(ht > WHeight / 2) {
             $(".paralax_title2").css({transform:"translateX(-"+(ht /100)*22 + "%)"});
         };
+        // 두번째 brandmission 텍스트 좌우스크롤 애니메이션 끝
 
-        if(ht-(text_animate_1-(wH/2))>= 1 ) {
-            $(".text_animate_1").stop().css({transform:"translateY(0)"});
+        // 두번째 카드애니메이션 텍스트 애니메이션
+        if(ht-(text_animate_4-(wH/2))>= 1 ) {
+            $(".text_animate_4").stop().css({transform:"translateY(0)"});
         }
 
-        if(ht-(text_animate_2-(wH/2))>= 1 ) {
-            $(".text_animate_2").stop().css({transform:"translateY(0)"});
+        if(ht-(text_animate_5-(wH/2))>= 1 ) {
+            $(".text_animate_5").stop().css({transform:"translateY(0)"});
         }
+        // 두번째 카드애니메이션 텍스트 애니메이션 끝
 
         if(ht > (WHeight / 2) + (header * 5)) {
             $(".tit_holder_left .txt_mask:nth-child(2) span").delay(300).queue(function(next){
@@ -172,7 +183,10 @@ $(function(){
 
         // we are cenno 텍스트 애니메이션 시작
         if(ht-(comment-(wH/2))>= 1) {
-            $('.comment p').stop().css({opacity:"1", transform:"translateY(0)"})
+            $('.comment p').delay(300).queue(function(next){
+                $(this).stop().css({opacity:"1", transform:"translateY(0)"});
+                next();
+            });
         } else {
             $('.comment p').stop().css({opacity:"0", transform:"translateY(100%)"});
         }
@@ -211,5 +225,105 @@ $(function(){
         }
         // we are cenno 두번째 카드 애니메이션 끝
         
+
+
+        // 미디어 쿼리 스크립트 시작
+        var maxwidth = matchMedia("screen and (max-width:766px)");
+
+        if(maxwidth.matches) {
+            // 첫번째, 두번째, 세번째 카드 애니메이션 시작
+            if(ht-(animate_1-(wH/2))>= 1 ) {
+                $(".card1_1").stop().css({opacity:"1", transform:"translate3d(0,0,0)"});                
+            } else {
+                $(".container_columns:nth-child(1) .txt_motion .txt_mask span").stop().css({transform:"translateY(120%)"});
+            };
+
+            if(ht-(animate_2-(wH/2))>= 1 ) {
+                $(".card1_2").stop().css({opacity:"1", transform:"translate3d(0,0,0)"});                
+            } else {
+                $(".container_columns:nth-child(2) .txt_motion .txt_mask span").stop().css({transform:"translateY(120%)"});
+            };
+
+            if(ht-(animate_3-(wH/2))>= 1 ) {
+                $(".card1_3").stop().css({opacity:"1", transform:"translate3d(0,0,0)"});                
+            } else {
+                $(".container_columns:nth-child(3) .txt_motion .txt_mask span").stop().css({transform:"translateY(120%)"});
+            }; 
+            // 첫번째, 두번째, 세번째 카드 애니메이션 끝
+
+
+            // 두번째 brandmission 텍스트 좌우스크롤 애니메이션
+            if(ht-(sec0_2-(wH/2))>= 1) {
+                $(".paralax_title2").css({transform:"translateX(-"+(+(ht-(sec0_2-(wH/2))) /2) +"%)"});
+            };
+            // 두번째 brandmission 텍스트 좌우스크롤 애니메이션 끝
+
+            // 카드 텍스트 애니메이션
+            if(ht-(text_wrap1_1-(wH/2))>=-200) {
+            $(".card1_1 .text_animate_1 p").stop().css({transform:"translateY(0)"});
+            $(".card1_1 .text_wrap h3").delay(300).queue(function(next){
+                $(this).stop().css({transform:"translateY(0)"});
+                next();
+            });
+            $(".card1_1 .text_wrap .text_animate_3 p").delay(600).queue(function(next){
+                $(this).stop().css({transform:"translateY(0)"});
+                next();
+            });
+            
+            } else {
+                $(".card1_1 .text_animate_1 p").stop().css({transform:"translateY(100%)"});
+                $(".card1_1 .text_wrap h3").stop().css({transform:"translateY(100%)"});
+                $(".card1_1 .text_wrap .text_animate_3 p").stop().css({transform:"translateY(121%)"});
+            }
+
+            if(ht-(text_wrap1_2-(wH/2))>=-200) {
+                $(".card1_2 .text_animate_1 p").stop().css({transform:"translateY(0)"});
+            $(".card1_2 .text_wrap h3").delay(300).queue(function(next){
+                $(this).stop().css({transform:"translateY(0)"});
+                next();
+            });
+            $(".card1_2 .text_wrap .text_animate_3 p").delay(600).queue(function(next){
+                $(this).stop().css({transform:"translateY(0)"});
+                next();
+            }); 
+            } else {
+                $(".card1_2 .text_animate_1 p").stop().css({transform:"translateY(100%)"});
+                $(".card1_2 .text_wrap h3").stop().css({transform:"translateY(100%)"});
+                $(".card1_2 .text_wrap .text_animate_3 p").stop().css({transform:"translateY(121%)"});
+            }
+
+            if(ht-(text_wrap1_3-(wH/2))>=-200) {
+                $(".card1_3 .text_animate_1 p").stop().css({transform:"translateY(0)"});
+                $(".card1_3 .text_wrap h3").delay(300).queue(function(next){
+                    $(this).stop().css({transform:"translateY(0)"});
+                    next();
+                });
+                $(".card1_3 .text_wrap .text_animate_3 p").delay(600).queue(function(next){
+                    $(this).stop().css({transform:"translateY(0)"});
+                    next();
+                });
+            } else {
+                $(".card1_3 .text_animate_1 p").stop().css({transform:"translateY(100%)"});
+                $(".card1_3 .text_wrap h3").stop().css({transform:"translateY(100%)"});
+                $(".card1_3 .text_wrap .text_animate_3 p").stop().css({transform:"translateY(121%)"});
+            }
+            // 카드 텍스트 애니메이션 끝
+            
+
+            if(ht-( text_animate_4-(wH/2))>= 1 ) {
+                $(".text_animate_4").stop().css({transform:"translateY(0)"});
+            } else {
+                $(".text_animate_4").stop().css({transform:"translateY(120%)"});
+            }
+
+            if(ht-( text_animate_5-(wH/2))>= 1 ) {
+                $(".text_animate_5").stop().css({transform:"translateY(0)"});
+            } else {
+                $(".text_animate_5").stop().css({transform:"translateY(120%)"});
+            }
+        } 
     });
+
+
+    
 });
