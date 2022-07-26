f9540ef4962bdf02e2f7dbbd0eb0c07067179b68
justice725 <84830256+justice725@users.noreply.github.com>
2022-07-26 17:01:15 +0900

    Add files via upload

--- js/greet.js
+++ js/greet.js
@@ -1,52 +1,80 @@
 $(function(){
     $(window).on('scroll', function(){
         const ht = $(window).scrollTop();
-        const deviceHt = $(window).outerHeight(true);
-        const PTF = $('.page_center').offset().top;
-        const Bd = $('.text_box .bold').offset().top;
-        const Com = $('.comment').offset().top;
+        const wH = $(window).height();
+        const page_title_flex = $(".page_title_flex").offset().top;
+        const bold_ani_1 = $(".bold_ani_1").offset().top; // 타이틀 애니메이션
+        const bold_ani_2 = $(".bold_ani_2").offset().top; // 타이틀 애니메이션
 
+        const com_ani_1 = $(".com_ani_1").offset().top; // 코멘트 애니메이션
+        const com_ani_2 = $(".com_ani_2").offset().top; // 코멘트 애니메이션
+        const com_ani_3 = $(".com_ani_3").offset().top; // 코멘트 애니메이션
+        const com_ani_4 = $(".com_ani_4").offset().top; // 코멘트 애니메이션
+        const com_ani_5 = $(".com_ani_5").offset().top; // 코멘트 애니메이션
+
+
+        
+
+        // ceomessage 텍스트 좌우스크롤 애니메이션
         if(ht > 0) {
             $(".paralax_title").css({transform:"translateX(-"+ht / 20 + "%)"});
         };
+        // ceomessage 텍스트 좌우스크롤 애니메이션 끝
 
-        if(ht + (deviceHt / 2) > PTF) {
+        // 메인섹션 타이틀 애니메이션 
+        if(ht-(page_title_flex-(wH/2))>= 1) {
             $('.page_title_flex h2').stop().css({opacity:"1", transform:"translateY(0)"});
         } else {
             $('.page_title_flex h2').stop().css({opacity:"0", transform:"translateY(100%)"});
-        };
+        }
+        // 메인섹션 타이틀 애니메이션 끝
 
-        if(ht + (deviceHt / 2) > Bd) {
+        // bold 타이틀 애니메이션
+        if(ht-(bold_ani_1-(wH/2))>= 1) {
             $('.bold_ani_1').stop().css({opacity:"1", transform:"translateY(0)"});
-            $('.bold_ani_2').delay(300).queue(function(next){
-                $(this).stop().css({opacity:"1", transform:"translateY(0)"});
-                next();
-            });
+            
         } else {
-            $('.bold_ani_1, .bold_ani_2').stop().css({opacity:"0", transform:"translateY(100%)"});
+            $('.bold_ani_1').stop().css({opacity:"0", transform:"translateY(100%)"});
         };
 
-        if(ht + (deviceHt / 2) > Com) {
-            $('.com_ani_1').stop().css({opacity:"1", transform:"translateY(0)"});
-            $('.com_ani_2').delay(300).queue(function(next) {
-                $(this).stop().css({opacity:"1", transform:"translateY(0)"});
-                next();
-            });
-            $('.com_ani_3').delay(600).queue(function(next) {
-                $(this).stop().css({opacity:"1", transform:"translateY(0)"});
-                next();
-            });
-            $('.com_ani_4').delay(900).queue(function(next) {
-                $(this).stop().css({opacity:"1", transform:"translateY(0)"});
-                next();
-            });
-            $('.com_ani_5').delay(1200).queue(function(next) {
-                $(this).stop().css({opacity:"1", transform:"translateY(0)"});
-                next();
-            });
+        if(ht-(bold_ani_2-(wH/2))>= 1) {
+            $('.bold_ani_2').stop().css({opacity:"1", transform:"translateY(0)"});
         } else {
-            $('.com_ani_1, .com_ani_2, .com_ani_3, .com_ani_4, .com_ani_5').stop().css({opacity:"0", transform:"translateY(100%)"});
+            $('.bold_ani_2').stop().css({opacity:"0", transform:"translateY(100%)"});
+        }
+        // bold 타이틀 애니메이션 끝
+
+
+        // 코멘트 애니메이션 
+        if(ht-(com_ani_1-(wH/2))>= -200) {
+            $('.com_ani_1').stop().css({opacity:"1", transform:"translateY(0)"});
+        } else {
+            $('.com_ani_1').stop().css({opacity:"0", transform:"translateY(100%)"});
         }
 
+        if(ht-(com_ani_2-(wH/2))>= -200) {
+            $('.com_ani_2').stop().css({opacity:"1", transform:"translateY(0)"});
+        } else {
+            $('.com_ani_2').stop().css({opacity:"0", transform:"translateY(100%)"});
+        }
+
+        if(ht-(com_ani_3-(wH/2))>= -200) {
+            $('.com_ani_3').stop().css({opacity:"1", transform:"translateY(0)"});
+        } else {
+            $('.com_ani_3').stop().css({opacity:"0", transform:"translateY(100%)"});
+        }
+
+        if(ht-(com_ani_4-(wH/2))>= -200) {
+            $('.com_ani_4').stop().css({opacity:"1", transform:"translateY(0)"});
+        } else {
+            $('.com_ani_4').stop().css({opacity:"0", transform:"translateY(100%)"});
+        }
+
+        if(ht-(com_ani_5-(wH/2))>= -200) {
+            $('.com_ani_5').stop().css({opacity:"1", transform:"translateY(0)"});
+        } else {
+            $('.com_ani_5').stop().css({opacity:"0", transform:"translateY(100%)"});
+        }
+        // 코멘트 애니메이션 끝
     });
 });
--- js/map.js
+++ js/map.js
@@ -1,19 +1,30 @@
 $(function(){
     $(window).on('scroll', function(){
-        const WHeight = $('body').height();
         const ht = $(window).scrollTop();
         const PageWrapTop = $('.page_wrap').offset().top;
         const deviceHt = $(window).outerHeight(true);
         const CUHt = $('.context_us').offset().top;
-        const TBHt = $('.context_us table').offset().top;
-        
 
+        const wH = $(window).height();
+        
+        const map_flex = $(".map_flex").offset().top;
+        const left_title = $(".left_title").offset().top;
+        const right_top = $(".right_top").offset().top;
+        const right_bot = $(".right_bot").offset().top;
+        const context_us = $(".context_us").offset().top;
+        const table_animate_1 = $(".table_animate_1").offset().top;
+
+
+        // way to come 텍스트 좌우스크롤 애니메이션
         if(ht > 0) {
             $(".paralax_title").css({transform:"translateX(-"+ht / 20 + "%)"});
         }
+        // way to come 텍스트 좌우스크롤 애니메이션
 
-        if(ht > PageWrapTop) {
-            $('.left_title').stop().css({opacity:"1", transform:"translateY(0)"});
+        // mapflex 애니메이션
+        if(ht-(left_title-(wH/2))>= 1) {
+            $(".left_title h2").stop().css({opacity:"1", transform:"translateY(0)"});
+            /* $('.left_title').stop().css({opacity:"1", transform:"translateY(0)"});
             $('.right_top').delay(300).queue(function(next) {
                 $(this).stop().css({opacity:"1", transform:"translateY(0)"});
                 next();
@@ -21,22 +32,30 @@ $(function(){
             $('.right_bot').delay(600).queue(function(next) {
                 $(this).stop().css({opacity:"1", transform:"translateY(0)"});
                 next();
-            });
+            }); */
         } else {
-            $('.left_title').stop().css({opacity:"0", transform:"translateY(20%)"});
+            $('.left_title h2').stop().css({opacity:"0", transform:"translateY(20%)"});
+            /* $('.left_title').stop().css({opacity:"0", transform:"translateY(20%)"});
             $('.right_top').stop().css({opacity:"0", transform:"translateY(30%)"});
-            $('.right_bot').stop().css({opacity:"0", transform:"translateY(20%)"});
+            $('.right_bot').stop().css({opacity:"0", transform:"translateY(20%)"}); */
         };
 
-        if(ht + (deviceHt /2 ) >= CUHt) {
+        if(ht-(right_top-(wH/2))>= 1) {
+            $(".right_top_animate").stop().css({opacity:"1", transform:"translateY(0)"});
+        } else {
+            $(".right_top_animate").stop().css({opacity:"0", transform:"translateY(30%)"});
+        }
+
+        if(ht-(right_bot-(wH/2))>= 1) {
+            $("#map").stop().css({opacity:"1", transform:"translateY(0)"});
+        } else {
+            $("#map").stop().css({opacity:"0", transform:"translateY(20%)"});
+        }
+
+        if(ht-(context_us-(wH/2))>= 1) {
             $('.context_title').stop().css({opacity:"1", transform:"translateY(0)"});
-            $('.context_us table').delay(300).queue(function(next){
-                $(this).stop().css({opacity:"1", transform:"translateY(0)"});
-                next();
-            });
         } else {
             $('.context_title').stop().css({opacity:"0", transform:"translateY(200%)"});
-            $('.context_us table').stop().css({opacity:"0", transform:"translateY(100%)"});
         }
     });
 });
